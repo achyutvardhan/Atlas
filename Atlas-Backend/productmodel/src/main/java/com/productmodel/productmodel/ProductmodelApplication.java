6b8465790f43088345d974f506d9aff918937a78
achyutvardhan <achyutvardhan234@gmail.com>
2025-10-11 22:32:58 +0530

    microservice is setup is done

--- Atlas-Backend/productmodel/src/main/java/com/productmodel/productmodel/ProductmodelApplication.java
+++ Atlas-Backend/productmodel/src/main/java/com/productmodel/productmodel/ProductmodelApplication.java
@@ -2,8 +2,10 @@ package com.productmodel.productmodel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductmodelApplication {
 
 	public static void main(String[] args) {
