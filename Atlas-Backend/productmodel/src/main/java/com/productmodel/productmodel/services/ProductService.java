3930b35baccf1f0e133b0aa04aaa3697a2d5b4fa
achyutvardhan <achyutvardhan234@gmail.com>
2025-11-28 00:44:19 +0530

    Added Redistemplet in cofig

--- Atlas-Backend/productmodel/src/main/java/com/productmodel/productmodel/services/ProductService.java
+++ Atlas-Backend/productmodel/src/main/java/com/productmodel/productmodel/services/ProductService.java
@@ -8,10 +8,9 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.productmodel.productmodel.dto.Productdto;
@@ -27,6 +26,9 @@ public class ProductService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @CachePut(key = "#category")
     public List<Productdto> getProductsByCategory(String category) {
@@ -44,32 +46,33 @@ public class ProductService {
         Productdto productdto = modelMapper.map(product, Productdto.class);
         return productdto;
     }
+
     @Cacheable(key = "'all-products'")
     public List<Productdto> getAllProducts() {
         List<Product> products = productRepository.findAll();
 
         if (products.isEmpty())
             return Collections.emptyList();
-        ;
         return products.stream().map(p -> modelMapper.map(p, Productdto.class)).toList();
     }
-    @Caching(
-        evict = {@CacheEvict(key = "'all-products'", allEntries = true),},
-        put = {@CachePut(key = "'all-products'")}
-    )
+
     public Productdto addProduct(Productdto productdto) {
         Product product = modelMapper.map(productdto, Product.class);
         if (product == null)
             return null;
         product.setProductDoa(new Date(System.currentTimeMillis()));
         Product saved = productRepository.save(product);
+
+        String key = "products::all-products";
+        @SuppressWarnings("unchecked")
+        List<Productdto> cachedProductdtos = (List<Productdto>) redisTemplate.opsForValue().get(key);
+        if (cachedProductdtos != null) {
+            cachedProductdtos.add(modelMapper.map(saved, Productdto.class));
+            redisTemplate.opsForValue().set(key, cachedProductdtos);
+        }
         return modelMapper.map(saved, Productdto.class);
     }
-    
-    @Caching(
-        evict = {@CacheEvict(key = "'all-products'", allEntries = true),},
-        put = {@CachePut(key = "'all-products'")}
-    )
+
     public updateStockDto updateStock(UUID productId, int quantityOrdered) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
